ea67ecd18fdc5c9fe135faf8fe94d6a1b4b25c78
claudiuc <claudiu.chirilov@dreamproduction.ro>
2017-08-09 15:29:25 +0300

    Initialized the repo.

---
+++ src/Version.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace DreamProduction\Composer;
+
+use Composer\Composer;
+use Composer\Util\ProcessExecutor;
+
+
+class Version extends PluginInterface, EventSubscriberInterface {
+
+	public function activate(Composer $composer, IOInterface $io) {
+	    $this->composer = $composer;
+	    $this->io = $io;
+	    $this->eventDispatcher = $composer->getEventDispatcher();
+	    $this->executor = new ProcessExecutor($this->io);
+    }
+
+	public static function getSubscribedEvents() {
+	    return array(
+	        'init' => 'checkDrupalVersion',
+	    );
+	}
+
+	public function checkDrupalVersion(Event $event) {
+
+	}
+}
