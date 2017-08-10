2e452109ef135a85b12666820cee8a637274b29f
claudiuc <claudiu.chirilov@dreamproduction.ro>
2017-08-10 12:12:22 +0300

    Fixed latest bugs.

--- src/Version.php
+++ src/Version.php
@@ -50,10 +50,30 @@ class Version implements PluginInterface, EventSubscriberInterface {
 	}
 
 	public function checkDrupalVersion(Event $event) {
+    $this->io->write('<warning>Checking latest stable Drupal version...</warning>');
     // Get the latest stable Drupal version
     $this->getDrupalStableVersion();
-		$this->io->write('<info>Drupal version check works.</info>');
 	}
+  
+  protected function getInstalledDrupalVersion() {
+    try {
+      $repositoryManager = $this->composer->getRepositoryManager();
+      $localRepository = $repositoryManager->getLocalRepository();
+      $packages = $localRepository->getPackages();
+      $drupalVersion = FALSE;
+      
+      foreach ($packages as $package) {
+        if ($package->getName() == 'drupal/core') {
+          $drupalVersion = $package->getVersion();
+        }
+      }
+     
+      return $drupalVersion;
+    }
+    catch (Exception $ex) {
+      
+    }
+  }
   
   protected function getDrupalStableVersion() {
     try {
@@ -61,12 +81,29 @@ class Version implements PluginInterface, EventSubscriberInterface {
       $html =  HtmlDomParser::file_get_html('https://www.drupal.org/project/drupal');
       
       // Find the first release row (assume it's Drupal 8, as being the most important now)
-      if ($release_div = $html->find('div.pane-project-downloads-recommended div.views-content div.views-row div.views-field-field-release-version div.field-content', 0)) {
+      if ($release_div = $html->find('div.pane-project-downloads-recommended div.view-content div.views-row-1 div.views-field-field-release-version div.field-content')) {
         // Get the release text title
-        $release_h4_text = $release_div->find('h4', 0)->text;
-        $this->io->write('<info>Drupal version: ' . trim(str_replace(Version::DRUPAL_CORE_PREFIX_TEXT, '', $release_h4_text)) . '</info>');
+        $release_h4_text = $release_div[0]->find('h4', 0)->innertext;
+        $latestStableVersion = trim(str_replace(Version::DRUPAL_CORE_PREFIX_TEXT, '', $release_h4_text)) . '.0'; // Append .0 to fully compare with installed package that have 4 digits in the version
+        
+        $this->io->write('<info>Latest stable Drupal version: <warning>' . $latestStableVersion . '</warning></info>');
+        
+        // Get installed version of the package
+        $current_drupal_version = trim(strtolower($this->getInstalledDrupalVersion()));
+        
+        // Compare versions
+        if (version_compare(strtolower($latestStableVersion), $current_drupal_version) == 0 ) {
+          $this->io->write('<info>You are up-to-date with your Drupal version.</info>');
+        }
+        else {
+          $this->io->write('<error>You are not running the latest stabled version. Currently installed version is ' . $current_drupal_version . '</error>');
+        }
+        
         
         return $release_h4_text;
+      }
+      else {
+        $this->io->write('<error>No release found :( Please check the plugin and inform the author.</error>');
       }
       
       return NULL;
